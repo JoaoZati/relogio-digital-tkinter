e24f9e8b8383a533dc995298ce8db6a068a70d26
JoaoZati <joaozati@gmail.com>
2021-09-28 15:59:45 -0300

    Commit 3: - Adicionando fonte digital. - Primeiro usar o DS-DIGI.TTF, instalar na maquina e depois rodar com ds-digital no config do tkinter

--- relogio_digital.py
+++ relogio_digital.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from datetime import datetime
+import pyglet
+
+pyglet.font.add_file('digital-7.ttf')
 
 COR1 = "#3d3d3d"  # preta
 COR2 = "#fafcff"  # branca
@@ -17,13 +20,17 @@ class RelogioDigital:
         self.window.resizable(0, 0)
         self.window.config(bg=COR1)
 
-        self.label_1 = tk.Label(self.window, text="00:00:00", font=('Arial', 70),
-                                bg=COR1, fg=COR6)
-        self.label_1.grid(row=0, column=0, sticky='nw', padx=5)
+        self.window.rowconfigure(0, weight=3)
+        self.window.rowconfigure(1, weight=1)
+        self.window.columnconfigure(0, weight=1)
 
-        self.label_2 = tk.Label(self.window, text="00:00:00", font=('Arial', 20),
+        self.label_1 = tk.Label(self.window, text="00:00:00", font=('ds-digital', 80),
                                 bg=COR1, fg=COR6)
-        self.label_2.grid(row=1, column=0, sticky='nw', padx=5)
+        self.label_1.grid(row=0, column=0, padx=0, pady=0)
+
+        self.label_2 = tk.Label(self.window, text="00:00:00", font=('ds-digital', 20),
+                                bg=COR1, fg=COR6)
+        self.label_2.grid(row=1, column=0, sticky='nw', padx=0)
 
         self.relogio()
 
